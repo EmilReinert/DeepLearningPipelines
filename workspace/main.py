3e8fd25196f7b8e48afff7785a7be7e524383b68
Emil Reinert <EmilReinert>
2019-01-03 21:54:56 +0100

    adding testing data and icluding prediction algortihm. i dont know how to make it work tho...

--- workspace/main.py
+++ workspace/main.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     """
     Main to test-run specific pipelines or modules
     """
-    dp = dpLSTM.DefectPredictor('/home/emil/Documents/DeepLearningProject/PaperImplementation/DeepLearningPipelines/workspace/data/defective_test.py',
-                            '/home/emil/Documents/DeepLearningProject/PaperImplementation/DeepLearningPipelines/workspace/data/clean_test.py')
+    dp = dpLSTM.DefectPrediction('/home/emil/Documents/DeepLearningProject/PaperImplementation/DeepLearningPipelines/workspace/data/defective_test.py',
+                                 '/home/emil/Documents/DeepLearningProject/PaperImplementation/DeepLearningPipelines/workspace/data/clean_test.py',
+                                 '/home/emil/Documents/DeepLearningProject/PaperImplementation/DeepLearningPipelines/workspace/data/testdata_test.py')
     dp.run()
-    print("hi")
+    print("run completed")
--- workspace/pipelines/defectPrediction_deepTreeLstm.py
+++ workspace/pipelines/defectPrediction_deepTreeLstm.py
@@ -2,6 +2,8 @@ import os
 import sys
 import re
 import ast  # for python AST respresentation
+import random
+import torch
 
 
 def code2ast(code):
@@ -14,10 +16,10 @@ def code2ast(code):
     """
     def transform_ast(code_ast):
         if isinstance(code_ast, ast.AST):
-            node = {to_camelcase(k): transform_ast(
+            ast_node = {to_camelcase(k): transform_ast(
                 getattr(code_ast, k)) for k in code_ast._fields}
-            node['node_type'] = to_camelcase(code_ast.__class__.__name__)
-            return node
+            ast_node['node_type'] = to_camelcase(code_ast.__class__.__name__)
+            return ast_node
         elif isinstance(code_ast, list):
             return [transform_ast(el) for el in code_ast]
         else:
@@ -60,65 +62,182 @@ def tidy(code):
     return code
 
 
-def generate_ast_dict(root):
+def create_dictionary(dataset, max_count):
     """
-    extracts a
-    """
-    pass
-
-
-def extract_highest_occurences(dataset, max_count):
-    """
-    Builds a fixed sized dictionary by iterating over dataset.
+    Builds a fixed sized dictionary, with an <UNK> entry at last position
 
     :param dataset: tokenized list, optionally n dimensional
     :param max_count: defines how long our dictionary will be
     :return: top 'max_count' tokens of dictionary as a list
     """
-    # global fixed size dictionary as set
-    global_dict = {}
-    # iterating over all training files
-    for data in dataset:
-        # a complete dictionary for one file
-        local_dict = {}
-        # iterating over all words of a file
-        for w in ast.walk(data):
-            # if word not in the local dictionary then we add it
-            # otherwise we rise count
-            if w in local_dict:
-                local_dict[w] += 1
-            else:
-                local_dict[w] = 1
-        # local dict counts will now be merged into fix sized, global dictionary
-        for w in local_dict:
-            if w in global_dict:
-                global_dict[w] += local_dict[w]
-            else:
-                global_dict[w] = local_dict[w]
 
-        # global dict will be filled with highest counts
-        # first we find highest count
-        highest_count = 0
-        for w in global_dict:
-            if highest_count < global_dict[w]:
-                highest_count = global_dict[w]
-        # now we create an updated highest count global dict
-        new_global_dict = {}
-        while len(new_global_dict) < max_count:
-            if highest_count < 1:
-                break
-            # filling new global
-            for w in global_dict:
-                if (global_dict[w] == highest_count and
-                        len(new_global_dict) < max_count):
-                    new_global_dict[w] = highest_count
-            highest_count -= 1
-        global_dict = new_global_dict
-    # print("maxcout", global_dict)
-    return list(global_dict)
+    def extract_highest_occurences(dataset, max_count):
+        """
+        finds highest token occurences in datasets
+
+        :param dataset: tokenized list, optionally n dimensional
+        :param max_count: defines how long our dictionary will be
+        :return: top 'max_count' tokens of dictionary as a list
+        """
+        # global fixed size dictionary as set
+        global_dict = {}
+        # iterating over all training files
+        for tree in dataset:
+            # a complete dictionary for one file
+            local_dict = {}
+            # iterating over all words of a file
+            for ast_node in ast.walk(tree):
+                # if word not in the local dictionary then we add it
+                # otherwise we rise count
+                if ast_node in local_dict:
+                    local_dict[ast_node] += 1
+                else:
+                    local_dict[ast_node] = 1
+            # local dict counts will now be merged into fix sized, global dictionary
+            for ast_node in local_dict:
+                if ast_node in global_dict:
+                    global_dict[ast_node] += local_dict[ast_node]
+                else:
+                    global_dict[ast_node] = local_dict[ast_node]
+
+            # global dict will be filled with highest counts
+            # first we find highest count
+            highest_count = 0
+            for ast_node in global_dict:
+                if highest_count < global_dict[ast_node]:
+                    highest_count = global_dict[ast_node]
+            # now we create an updated highest count global dict
+            new_global_dict = {}
+            while len(new_global_dict) < max_count:
+                if highest_count < 1:
+                    break
+                # filling new global
+                for ast_node in global_dict:
+                    if (global_dict[ast_node] == highest_count and
+                            len(new_global_dict) < max_count):
+                        new_global_dict[ast_node] = highest_count
+                highest_count -= 1
+            global_dict = new_global_dict
+        # print("maxcout", global_dict)
+        return list(global_dict)
+
+    dictionary = extract_highest_occurences(dataset, max_count-1)
+    dictionary.append("UNK")
+    return dictionary
 
 
-class DefectPredictor:
+def random_embed(dictionary, vector_length):
+    """
+    embeds dictionary with random valued vectors for initializing random weights.
+    values lay between -1 and 1
+
+    :param dictionary: 2b-embedded dictionary
+    :param vector_length: desired length for vectors
+    :returns: embedded dictionary as matrix
+    """
+    e_mat = []
+    # create matric with dimension dictionarysize x vector length
+    for mi in range(len(dictionary)):
+        e_vec = []
+        for vi in range(vector_length):
+            e_vec.append(random.uniform(-1.0, 1.0))
+        e_mat.append(e_vec)
+    return e_mat
+
+
+def ast2vec(ast_node, dictionary, embed_matrix):
+    """
+    embedding of single ast node 
+
+    :param ast_token: 2b-embedded ast  ast_node 
+    :param dictionary: dictionary of datatokens
+    :param embed_matrix: embedding material
+    :returns: vector representation of ast
+    """
+    # find index first
+    if ast_node in dictionary:
+        index = dictionary.index(ast_node)
+    else:
+        # last element in dictionary is Unknown type; equals dictionary.index("UNK")
+        index = len(dictionary)-1
+    # lookup index in embedding matrix
+    return embed_matrix[index]
+
+
+def embed(datasets, dictionary, embed_matrix):
+    """
+    embedding of ast datasets with dictionary and embedding matrix
+
+    :param dataset: tokenized list, optionally n dimensional
+    :param dictionary: dictionary of datatokens
+    :param embed_matrix: embedding material
+    :returns: vector representation of datafiles
+    """
+    embedded_datasets = []
+    for tree in datasets:
+        embedded_data = []
+        for ast_node in ast.walk(tree):
+            embedded_data.append(ast2vec(ast_node, dictionary, embed_matrix))
+        embedded_datasets.append(embedded_data)
+    return embedded_datasets
+
+
+class PredictorLSTM:
+    """
+    The actual NN Module for all learning and testing processes for DefectPrediction.
+    Will be able to do defect predictions for one file(one AST)
+    """
+
+    def __init__(self, dictionary, embedding_matrix):
+        """
+        :param dictionary: dictionary of datatokens
+        :param embed_matrix: embedding material
+        """
+        self.dictionary = dictionary
+        self.emb_matrix = embedding_matrix
+        self.vec_length = len(embedding_matrix[0])
+        # TODO LSTM tweaks
+
+    def tree_lstm(self, ast_node):
+        """
+        Process of one LSTM unit.
+        Recursively calls learning processes on all children in one tree
+
+        :param ast_node: one Python AST node; First call will be with root Node
+        :returns: hidden state and context of node; eventually for the whole AST
+        """
+        weight = 0  # TODO weights with lstm calculation!!
+        w_t = ast2vec(ast_node, self.dictionary,
+                      self.emb_matrix)  # embedding of tree
+        # sum of children hidden outputs
+        h_ = [0]
+        # child hidden state
+        h_k = [0]
+        # context of child
+        c_k = [0]
+        # forget gates
+        f_tk = 0
+        # childrem forgetrates times the context
+        c_ = 0
+        for k in ast.iter_child_nodes(ast_node):
+            h_k, c_k = self.tree_lstm(k)
+            f_tk = torch.nn.Sigmoid()(weight)
+            h_ += h_k
+            c_ += (f_tk * c_k)
+        # input gate
+        i_t = torch.nn.Sigmoid()(weight)
+        # vector of new candidate values for t
+        c_t_ = torch.nn.Tanh()(weight)
+        # context
+        c_t = i_t * c_t_ + c_
+        # output gate
+        o_t = torch.nn.Sigmoid()(weight)
+        h_t = o_t * torch.nn.Tanh()(c_t)
+
+        return h_t, c_t
+
+
+class DefectPrediction:
     """
     implementation Attemt to a published Paper: 
     'A deep tree-based model for software defect prediction' 
@@ -127,41 +246,59 @@ class DefectPredictor:
     TASK: Predicting Probability of a Code Being Defective or not
     """
 
-    def __init__(self, data_defective, data_clean):
+    def __init__(self, data_defective, data_clean, data_test):
         """
         :param data_defective: path to datacorpus code labled as defective
         :param data_clean: path to datacorpus code labled as clean
         """
         self.data_defective = data_defective
         self.data_clean = data_clean
+        self.data_test = data_test
         self.voc_size = 5
+        self.vec_length = 3
 
     def run(self):
         """
         runs whole Pipeline with already initialized defective and clean datasets
         """
-        ########### PREPROCESSING ###########
+    # PREPROCESSING ###########
 
-        # cleaning and opening files
+    # cleaning and opening files TODO manage datacorpus with lables and crawling
         data_def = tidy(read_file(self.data_defective))
         data_cln = tidy(read_file(self.data_clean))
+        data_test = tidy(read_file(self.data_test))
 
-        # transforming file strings to AST
+    # transforming file strings to AST
         # parsing with own funciton ast_data_def_exp = code2ast(data_def) TODO manage error/empty files
         # parsing with ast.parse
         ast_data_def = ast.parse(data_def)
         ast_data_cln = ast.parse(data_cln)
+        ast_data_test = ast.parse(data_test)
 
         # prints
-        print(ast.dump(ast_data_def))
-        print(ast.dump(ast_data_cln))
+        print("Defective Data AST:\n", ast.dump(ast_data_def))
+        print("Clean Data AST:\n", ast.dump(ast_data_cln))
+        print("Test Data AST:\n", ast.dump(ast_data_test))
 
-        # vocabulary of highest occurences
-        self.dictionary = extract_highest_occurences(
-            [ast_data_cln, ast_data_def], self.voc_size)  # TODO manage whole datastorage TODO unkown datatype?
+    # vocabulary of highest occurences
+        self.dictionary = create_dictionary(
+            [ast_data_cln, ast_data_def], self.voc_size)  # TODO manage whole datastorage
 
-        # EMBEDDING ########### TODO
+    # EMBEDDING ########### TODO learning?
+        # random embedding of dictionary; as initializing!
+        self.emb_matrix = random_embed(self.dictionary, self.vec_length)
+        # print("Embedding Matrix:\n", self.emb_matrix)
 
-        # NEURAL NETWORK ########### TODO
+        # embed datasets
+        # TODO: is embedding the whole datasets suitable or modificable for this paper?
+        def_emb, cln_emb = embed(
+            [ast_data_def, ast_data_cln], self.dictionary, self.emb_matrix)
+        # print("Embedded Defective Data:\n", def_emb)
+        # print("Embedded Clean Data:\n", cln_emb)
 
-        # RESULTS ########### TODO
+    # NEURAL NETWORK ########### TODO
+        # obtaining hiddenstate and context of all trees(files) from training data
+        self.predictor = PredictorLSTM(self.dictionary, self.emb_matrix)
+        h_root, c_root = self.predictor.tree_lstm(ast_data_test)
+
+    # RESULTS ########### TODO
